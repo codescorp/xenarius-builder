e19c4afc5d6f2edcc45b935840bbdf201e4e262c
codescorp <arpan.mukhopadhyay@rebaca.com>
2022-02-17 21:26:16 +0530

    added comment

--- vars/buildLib.groovy
+++ vars/buildLib.groovy
@@ -1,5 +1,10 @@
 #!/usr/bin/env groovy
 
+/**
+ * This is the entrypoint of the pipeline script. This library
+ * can have multiple scripts and each of them can have a <code>call()</code>
+ * method.
+ */
 def call() {
   println "My first pipeline script. Feeling awesome"
 }
